bf74aa2869d52f77f4952aff3eabdf7f55b4db6b
drgy <drgonjak@gmail.com>
2022-05-03 19:12:25 +0200

    Update build

--- update_paths.js
+++ update_paths.js
@@ -1,4 +1,4 @@
 import fs from 'fs';
 
 const index = fs.readFileSync('build/index.html', 'utf8');
-fs.writeFileSync('build/index.html', index.replace(/href="\//g, 'href="'));
+fs.writeFileSync('build/index.html', index.replace(/\/_app/g, './_app'));
